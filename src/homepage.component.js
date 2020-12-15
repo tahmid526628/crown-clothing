9f0c0653fa46aed834c7f207fe9a5db2743a2f0f
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2020-12-16 02:13:28 +0600

    created a ecommerce app and it is the first commit. some work with sass and homepage.

---
+++ src/homepage.component.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    return(
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='sub-tille'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>JACKETS</h1>
+                        <span className='sub-tille'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>SNEAKERS</h1>
+                        <span className='sub-tille'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>WOMEN</h1>
+                        <span className='sub-tille'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>MEN</h1>
+                        <span className='sub-tille'>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
